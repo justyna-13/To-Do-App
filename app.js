99c2da78352c15997638916da7292703375d6359
Justyna <gierackajustyna@gmail.com>
2019-04-18 09:50:04 +0200

    added server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+const port = process.env.port || 5000;
+app.listen(port, () => console.log(`Listening on ${port}`));
