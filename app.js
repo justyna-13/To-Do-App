b149f2952c11ad0189464e73413c12e153741100
Mikolaj_Kaminski <hesoyam1298@gmail.com>
2019-04-19 12:17:06 +0200

    linked style.css and buttons

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use('/public', express.static('public'));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
