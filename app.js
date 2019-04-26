d429f2c3b8c5bc6a2f1dde8f124f380921d9a94f
Justyna <gierackajustyna@gmail.com>
2019-04-26 14:00:32 +0200

    login and register

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+
 const app = express();
-const path = require('path');
-const logged = require('./routes/logged');
-const register = require('./routes/register');
-const log = require('./routes/log');
-const dashboard = require('./routes/dashboard');
+
+require('./config/passport')(passport);
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(
+        db, {
+            useNewUrlParser: true
+        }
+    )
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+app.set('view engine', 'ejs');
 
 app.use('/public', express.static('public'));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(express.urlencoded({
+    extended: true
+}));
 
-app.use(express.json());
-app.use('/', log);
-app.use('/logged', logged);
-app.use('/register', register);
-app.use('/dashboard', dashboard);
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
+    })
+);
 
-const port = process.env.port || 5000;
-app.listen(port, () => console.log(`Listening on ${port}`));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, console.log(`Listening on port ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://user-piotr:' + encodeURIComponent('mocnehaslo') + '@to-do-app-wyfbz.gcp.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: dbPassword
+};
